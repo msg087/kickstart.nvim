2d8d998f8995d361934bcdc33a243ab42963d4b2
msg087 <123316262+msg087@users.noreply.github.com>
2024-06-22 09:00:47 -0400

    update color scheme to work correctly with material

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,8 +1,23 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- {
+  --   vim.cmd 'let g:material_style="deep ocean"',
+  -- },
+  {
+    'marko-cerovac/material.nvim',
+    priority = 1000,
+    opts = {},
+    init = function()
+      -- vim.cmd 'let g:material_style="deep ocean"'
+      -- vim.cmd 'let g:material_style="lighter"'
+      vim.cmd 'let g:material_style="darker"'
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+
+      vim.cmd 'colorscheme material'
+    end,
+  },
   { vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true }) },
   { 'wakatime/vim-wakatime', lazy = false },
   {
@@ -14,16 +29,17 @@ return {
     -- consider terrortylor/nvim-comment as well
     'tpope/vim-commentary',
   },
+  -- Database
+  { 'tpope/vim-dadbod', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
   {
-    -- Database
-    'tpope/vim-dadbod',
     'kristijanhusak/vim-dadbod-ui',
+    lazy = true,
     -- opts = {
     --   g.db_ui_execute_on_save=true,
     -- },
     -- }
     dependencies = {
-      { 'tpope/vim-dadbod', lazy = true },
+      { 'tpope/vim-dadbod', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
       { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
     },
     init = function()
@@ -36,22 +52,29 @@ return {
   },
   {
     'ThePrimeagen/harpoon',
+    lazy = true,
   },
   {
     'ThePrimeagen/vim-be-good',
+    lazy = true,
   },
   -- {
-  --   vim.cmd 'let g:material_style = "deep ocean"',
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
   -- },
-  { 'marko-cerovac/material.nvim', opts = {} },
+  -- { 'Mofiqul/vscode.nvim' },
+
   {
     'https://github.com/jamestthompson3/nvim-remote-containers',
+    lazy = true,
   },
   {
     'https://codeberg.org/esensar/nvim-dev-container',
     dependencies = 'nvim-treesitter/nvim-treesitter',
+    lazy = true,
   },
-
   -- disable netrw at the very start of your init.lua
   -- vim.g.loaded_netrw = 1
   -- vim.g.loaded_netrwPlugin = 1
