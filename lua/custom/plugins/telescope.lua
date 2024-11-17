76b36108bbfc198573b1b67dd2f0ca7028a9df4e
msg087 <michael@gannettsolutions.io>
2024-11-17 09:20:00 -0500

    update telescope config for dotfile search

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -74,11 +74,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sD', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+    vim.keymap.set('n', '<leader>sd', require('custom.custom_modules.my_telescope').edit_dotfiles, { desc = '[S]earch [D]otFiles' })
     -- vim.keymap.set('n', '<leader>sca', builtin.find_files, { desc = '[S]earch [C]ode [A]WS files' })
     -- vim.keymap.set('n', '<leader>sco', builtin.find_files, { desc = '[S]earch [C]ode [O]b files' })
 
